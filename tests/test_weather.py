639eba1860efdfc9cd15899c59582a1b8d175ac5
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-08-07 21:52:16 +1000

    Add `get_city_coordinates` method
    This method returns coordinates — latitude and
    longitude — of the given city.

---
+++ tests/test_weather.py
@@ -0,0 +1,18 @@
+import pytest
+
+from weerman.weather import get_city_coordinates
+
+
+def test_get_coordinates_of_valid_city():
+    city = "Владивосток"
+    coordinates = get_city_coordinates(city)
+
+    assert round(coordinates.latitude, 3) == 43.115
+    assert round(coordinates.longitude, 3) == 131.886
+
+
+def test_get_coordinates_of_invalid_city():
+    city = "Такого города не существует"
+
+    with pytest.raises(ValueError):
+        get_city_coordinates(city)
---
+++ weerman/weather.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+from functools import partial
+
+from geopy.geocoders import Nominatim
+
+Coordinates = namedtuple("Coordinates", ["latitude", "longitude"])
+
+
+def get_city_coordinates(city: str) -> Coordinates:
+    geolocator = Nominatim(user_agent="weerman")
+    geocode = partial(geolocator.geocode, language="ru")
+    location = geocode(city)
+    if location is None:
+        raise ValueError("City does not exist.")
+
+    return Coordinates(latitude=location.latitude, longitude=location.longitude)
