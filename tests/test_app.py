e649b66a17350eb71c66b6e5202ee838edae7b0b
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-08-08 09:44:04 +1000

    Add exceptions handling to `weather`

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from weerman.app import app as weerman_app
@@ -36,3 +37,14 @@ def test_weather_with_valid_city(client):
 def test_weather_with_no_city(client):
     response = client.get("/weather")
     assert response.status_code == 400
+
+
+def test_weather_with_non_existent_city(client):
+    response = client.get("/weather?city='Меня не существует'")
+    assert response.status_code == 404
+
+
+def test_weather_with_wrong_api_key(client):
+    os.environ["OPEN_WEATHER_API_KEY"] = "Wrong"
+    response = client.get("/weather?city=Владивосток")
+    assert response.status_code == 500
--- weerman/app.py
+++ weerman/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, abort
 
+from requests import RequestException
+
 from .coordinates import get_city_coordinates
 from .weather import get_weather_by_coordinates
 
@@ -8,11 +10,19 @@ app = Flask(__name__)
 
 @app.route("/weather")
 def weather():
-    if "city" not in request.args:
-        abort(400, "City must be specified.")
+    try:
+        city = request.args["city"]
+    except KeyError:
+        abort(400)
 
-    city = request.args.get("city")
-    coordinates = get_city_coordinates(city)
-    weather = get_weather_by_coordinates(coordinates)
+    try:
+        coordinates = get_city_coordinates(city)
+    except ValueError:
+        abort(404)
+
+    try:
+        weather = get_weather_by_coordinates(coordinates)
+    except RequestException:
+        abort(500)
 
     return weather._asdict()
